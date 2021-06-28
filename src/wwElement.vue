3c673c1d64dbed28e50ecf6682c71d3c41a1deb0
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-06-28 12:30:08 +0200

    recaptcha-v1

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,35 +1,74 @@
 <template>
-  <div class="my-element">
-    <div class="g-recaptcha" :data-sitekey="content.key"></div>
+  <div class="google-recaptcha">
+    <div
+      :id="`ww-recaptcha-${uid}`"
+      v-if="!isHidden"
+      v-show="content.key"
+    ></div>
+    <div v-if="!content.key">PLACEHOLDER</div>
   </div>
 </template>
 
 <script>
-// 6LduC9AaAAAAAJddSLqogxdLLD4qyzotZJuTpj6T
+// 6LfmLWAbAAAAAFIqia_-3o60fuIGuzXtJJuTVt0O : V2
 
 export default {
-  name: "__COMPONENT_NAME__",
   wwDefaultContent: {
-    key: "",
+    key: "6LfmLWAbAAAAAFIqia_-3o60fuIGuzXtJJuTVt0O",
   },
   props: {
     content: { type: "Object", required: true },
+    uid: { type: String, required: true },
   },
+  data() {
+    return {
+      isHidden: false,
+    };
+  },
+  /* wwEditor:start */
+  watch: {
+    "content.key"() {
+      this.isHidden = true;
+      this.$nextTick(() => {
+        this.isHidden = false;
+
+        this.$nextTick(() => {
+          this.render();
+        });
+      });
+    },
+  },
+  /* wwEditor:end */
   methods: {
-    initCaptcha() {
-      const scriptElement = document.createElement("script");
+    addScript() {
+      const functionName = `onRecaptchaLoad-${this.uid}`.replace(/-/g, "");
+      wwLib.getFrontWindow()[functionName] = this.render;
+
+      const scriptElement = wwLib.getFrontDocument().createElement("script");
       scriptElement.setAttribute(
         "src",
-        "https://www.google.com/recaptcha/api.js"
+        `https://www.google.com/recaptcha/api.js?onload=${functionName}&render=explicit`
       );
       scriptElement.setAttribute("type", "text/javascript");
       scriptElement.setAttribute("async", true);
       scriptElement.setAttribute("defer", true);
-      document.head.appendChild(scriptElement);
+      wwLib.getFrontDocument().head.appendChild(scriptElement);
     },
+    render() {
+      console.log("hello");
+      wwLib.getFrontWindow().grecaptcha.render(`ww-recaptcha-${this.uid}`, {
+        sitekey: this.content.key,
+        // theme: "dark",
+        // size: "compact",
+      });
+    },
+    // registerOnLoad() {
+    //   wwLib.getFrontWindow()[`onRecaptchaLoad-${this.uid}`] = this.render;
+    // },
   },
   mounted() {
-    this.initCaptcha();
+    // this.registerOnLoad();
+    this.addScript();
   },
 };
 </script>
