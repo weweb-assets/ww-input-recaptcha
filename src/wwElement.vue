de7a28471604ca6c0a35b0e9436bb9eef1d3f57b
Alexis JC <alexisjc.dev@gmail.com>
2022-03-18 19:59:34 +0100

    feat: implement new variable system

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -31,16 +31,17 @@ export default {
     },
     emits: ['trigger-event'],
     setup(props) {
-        const { value: variableValue, setValue } = wwLib.wwVariable.useComponentVariable(
-            props.uid,
-            'value',
-            props.content.value === undefined
-                ? {
-                      validate: false,
-                      code: null,
-                  }
-                : props.content.value
-        );
+        const { value: variableValue, setValue } = wwLib.wwVariable.useComponentVariable({
+            uid: props.uid,
+            name: 'value',
+            defaultValue: props.content.value,
+            sanitizer: value => {
+                if (value === undefined) {
+                    return { validate: false, code: null }
+                }
+                return value
+            }
+        });
 
         return { variableValue, setValue };
     },
